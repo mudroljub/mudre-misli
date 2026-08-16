b677edacadba1a324d60ac75c7f789cb55a893ec
Damjan Pavlica <mudroljub@gmail.com>
2026-08-17 00:28:58 +0200

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/mudre-misli' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mudre-misli' : '',
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
